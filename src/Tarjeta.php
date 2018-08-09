4c059c7cfc39f45b578dcfdfe716736dec2a9955
joseivaldi <ivaldi.josefina@gmail.com>
2018-08-09 15:01:33 -0300

    Creado archivo colectivo. Agregadas funciones a clase Tarjeta

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -4,14 +4,29 @@ namespace TrabajoTarjeta;
 
 class Tarjeta implements TarjetaInterface {
     protected $saldo;
+    protected $ID;
+
+    public function __construct(){
+      $this->saldo=0.0;
+      $this->ID=1;
+    }
 
     public function recargar($monto) {
-      // Esto esta hecho mal a proposito.
-      if ($monto % 2 == 0) {
-        $this->saldo += $monto;
-      }
+        
+        if($monto==10 || $monto==20 || $monto==30 || $monto==50 || $monto==100){
+          $this->saldo += $monto;
+        }
+        if($monto==510.15){
+          $this->saldo += $monto;
+          $this->saldo += 81.93;
+        }
+        if($monto==962.59){
+          $this->saldo += $monto;
+          $this->saldo += 221.58;
+        }
 
-      return $monto % 2 == 0;
+      //Esta parte no va, porque esta funcion es solo de recarga. Si quisieramos retornar el saldo, habria que hacer otra funcion.
+      //return $monto % 2 == 0;
     }
 
     /**
@@ -19,6 +34,7 @@ class Tarjeta implements TarjetaInterface {
      *
      * @return float
      */
+    
     public function obtenerSaldo() {
       return $this->saldo;
     }
