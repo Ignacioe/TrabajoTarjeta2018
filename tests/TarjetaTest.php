233923bcb1bfe39fd957742bbe6eadaa600a87cd
joseivaldi <ivaldi.josefina@gmail.com>
2018-08-09 15:55:19 -0300

    Testeo para cada monto de pago

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -17,6 +17,18 @@ class TarjetaTest extends TestCase {
 
         $this->assertTrue($tarjeta->recargar(20));
         $this->assertEquals($tarjeta->obtenerSaldo(), 30);
+
+        $this->assertTrue($tarjeta->recargar(50));
+        $this->assertEquals($tarjeta->obtenerSaldo(), 50);
+
+        $this->assertTrue($tarjeta->recargar(100));
+        $this->assertEquals($tarjeta->obtenerSaldo(), 100);
+
+        $this->assertTrue($tarjeta->recargar(510.15));
+        $this->assertEquals($tarjeta->obtenerSaldo(), 592.08);
+
+        $this->assertTrue($tarjeta->recargar(962.59));
+        $this->assertEquals($tarjeta->obtenerSaldo(), 1184.17);
     }
 
     /**
