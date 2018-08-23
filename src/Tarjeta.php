10bf83120c30425a00642b5f731d6901085e9a2a
Ignacioe <mypromega@hotmail.com>
2018-08-23 20:01:09 -0300

    correcion viaje plus

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -17,20 +17,20 @@ class Tarjeta implements TarjetaInterface {
 
     public function recargar($monto) {
         if($monto==10 || $monto==20 || $monto==30 || $monto==50 || $monto==100 && ($monto-$viajesPlus1-$viajesPlus2>0)){
-          $this->saldo += $monto-$viajesPlus1-$viajesPlus2;
+          $this->saldo += $monto-$this->viajesPlus1-$this->viajesPlus2;
           $this->viajesPlus1 = 0;
           $this->viajesPlus2 = 0;
           return TRUE;
         }
         if($monto==510.15){
-          $this->saldo += $monto-$viajesPlus1-$viajesPlus2;
+          $this->saldo += $monto-$this->viajesPlus1-$this->viajesPlus2;
           $this->saldo += 81.93;
           $this->viajesPlus1 = 0;
           $this->viajesPlus2 = 0;
           return TRUE;
         }
         if($monto==962.59){
-          $this->saldo += $monto-$viajesPlus1-$viajesPlus2;
+          $this->saldo += $monto-$this->viajesPlus1-$this->viajesPlus2;
           $this->saldo += 221.58;
           $this->viajesPlus1 = 0;
           $this->viajesPlus2 = 0;
