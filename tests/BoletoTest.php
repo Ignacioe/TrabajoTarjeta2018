a3adb8f2018e4840b7290cf891c44c03416751b9
joseivaldi <40646985+joseivaldi@users.noreply.github.com>
2018-10-20 18:33:45 -0300

    corrigiendo mas errores
    :(

--- tests/BoletoTest.php
+++ tests/BoletoTest.php
@@ -8,7 +8,10 @@ class BoletoTest extends TestCase {
 
     public function testSaldoCero() {
         $valor = 14.80;
-        $boleto = new Boleto($valor, NULL, NULL,NULL,NULL);
+        $tarjetaGratis = new Franquicia();
+        $colectivo = new Colectivo("142 Rojo", "Semtur", 10);
+
+        $boleto = new Boleto($valor,$colectivo, $tarjetaGratis,NULL,NULL);
 
         $this->assertEquals($boleto->obtenerValor(), $valor);
     }
