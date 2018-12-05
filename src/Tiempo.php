40a104508af849947569ed3adfe3ef76eea5e4bb
Ignacioe <mypromega@hotmail.com>
2018-12-05 17:12:51 -0300

    implemento el tiempo

--- src/Tiempo.php
+++ src/Tiempo.php
@@ -4,5 +4,26 @@ namespace TrabajoTarjeta;
 class Tiempo implements TiempoInterface {
 
     public $tiempoactual=0;
-
+    public $minutos=0;
+    public $hora=0;
+    public $dia=1;
+    
+    public function avanzar($cantMinutos){
+        if($this->tiempoactual+$cantMinutos>1439){
+            $this->dia+=intdiv($this->tiempoactual+$cantMinutos,1440);
+            $this->hora=0;
+            $this->minutos=0;
+            if($this->dia>365){
+                $this->dia-=365;
+            }
+            $this->tiempoactual+=($this->dia*1440);
+            $cantMinutos=($this->tiempoactual+$cantMinutos)%1440;
+        }
+        if($this->minutos+$cantMinutos>59){
+            $this->hora+=intdiv($this->minutos+$cantMinutos,60);
+            $this->minutos=($this->minutos+$cantMinutos)%60;
+        } else {
+            $this->minutos+=$cantMinutos;
+        }
+    }
 }
--- src/TiempoInterface.php
+++ src/TiempoInterface.php
@@ -2,5 +2,10 @@
 namespace TrabajoTarjeta;
 
 interface TiempoInterface {
-    
+    /**
+     * Avanza cierta cantidad de minutos.
+     *
+     * @param float $cantMinutos
+     */
+    public function avanzar($cantMinutos);
 }
