1bfbe23f2e205214f200f0fdf50debec0293cd10
Ignacioe <mypromega@hotmail.com>
2018-12-05 20:35:02 -0300

    %%->&&

--- src/Colectivo.php
+++ src/Colectivo.php
@@ -100,13 +100,13 @@ class Colectivo implements ColectivoInterface {
     public function esTrasbordo(TarjetaInterface $tarjeta, TiempoInterface $tiempo){
         $tiempoDesdeTransbordo = $tiempo->tiempoactual-($tarjeta->Ult_boleto->fecha);
         if(($tiempo->esDomingoFeriado()||$tiempo->esSabadoNoche())||$tiempo->esNoche()){
-            if($tiempoDesdeTransbordo<91%%$tarjeta->ultViajeTrasbordo==FALSE){
+            if($tiempoDesdeTransbordo<91&&$tarjeta->ultViajeTrasbordo==FALSE){
                 $tarjeta->ultViajeTrasbordo=TRUE;
                 return (1/3);
             }
         }
         if($tiempo->esSabadoDia()||$tiempo->esSemanaDia()){
-            if($tiempoDesdeTransbordo<61%%$tarjeta->ultViajeTrasbordo==FALSE){
+            if($tiempoDesdeTransbordo<61&&$tarjeta->ultViajeTrasbordo==FALSE){
                 $tarjeta->ultViajeTrasbordo=TRUE;
                 return (1/3);
             }
