40641b93db75712b430f15a1539f8d73bd150a3d
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2018-12-06 14:44:32 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/Colectivo.php
+++ src/Colectivo.php
@@ -28,92 +28,92 @@ class Colectivo implements ColectivoInterface {
 
     public function pagarCon(TarjetaInterface $tarjeta, TiempoInterface $tiempo) {
         
-        $fecha_actual=$tiempo->tiempoactual;
-        $multiplicador=1;
-        $ultimo_boleto=$tarjeta->ObtenerUltBol();
-        if($tarjeta->primerViaje==false){
-            if ($tarjeta->obtenerTipo()=="Medio") {
-                $multiplicador=$this->pagarConMedio($tarjeta,$tiempo,$ultimo_boleto,$fecha_actual);
+        $fecha_actual = $tiempo->tiempoactual;
+        $multiplicador = 1;
+        $ultimo_boleto = $tarjeta->ObtenerUltBol();
+        if ($tarjeta->primerViaje == false) {
+            if ($tarjeta->obtenerTipo() == "Medio") {
+                $multiplicador = $this->pagarConMedio($tarjeta, $tiempo, $ultimo_boleto, $fecha_actual);
                 
             }
-            if ($tarjeta->obtenerTipo()=="Gratis") {
-                $multiplicador=$this->pagarConFranquiciaTotal($tarjeta,$tiempo);
+            if ($tarjeta->obtenerTipo() == "Gratis") {
+                $multiplicador = $this->pagarConFranquiciaTotal($tarjeta, $tiempo);
             }
         } else {
-            $tarjeta->primerViaje=false;
-            if ($tarjeta->obtenerTipo()=="Medio") {
-                $multiplicador=0.5;
+            $tarjeta->primerViaje = false;
+            if ($tarjeta->obtenerTipo() == "Medio") {
+                $multiplicador = 0.5;
             }
-            if ($tarjeta->obtenerTipo()=="Gratis") {
-                $multiplicador=0;
+            if ($tarjeta->obtenerTipo() == "Gratis") {
+                $multiplicador = 0;
             }
         }
-        $multiplicador*=$this->esTrasbordo($tarjeta, $tiempo);
-        $precio_efectivo=$tarjeta->obtenerMonto()*$multiplicador;
+        $multiplicador *= $this->esTrasbordo($tarjeta, $tiempo);
+        $precio_efectivo = $tarjeta->obtenerMonto() * $multiplicador;
         
-        if ($this->puedePagarDosPlus($tarjeta,$tiempo,$precio_efectivo)) {
+        if ($this->puedePagarDosPlus($tarjeta, $tiempo, $precio_efectivo)) {
             $tarjeta->restarSaldo($precio_efectivo);
-            $normaloplus="Normal";
-            $pago="Abona 2 Viajes Plus";
-            $mult=3;
+            $normaloplus = "Normal";
+            $pago = "Abona 2 Viajes Plus";
+            $mult = 3;
         } else {
-            if ($tarjeta->obtenerPlus1()===FALSE) {
-                if ($tarjeta->obtenerSaldo()>=($precio_efectivo+$tarjeta->obtenerMonto())) {
+            if ($tarjeta->obtenerPlus1() === FALSE) {
+                if ($tarjeta->obtenerSaldo() >= ($precio_efectivo+$tarjeta->obtenerMonto())) {
                     $tarjeta->restarSaldo($precio_efectivo);
-                    $normaloplus="Normal";
-                    $pago="Abona 1 viaje plus";
-                    $mult=2;
+                    $normaloplus = "Normal";
+                    $pago = "Abona 1 viaje plus";
+                    $mult = 2;
                 } else {
-                    if ($tarjeta->obtenerPlus2()===FALSE) {
+                    if ($tarjeta->obtenerPlus2() === FALSE) {
                         return FALSE;
                     }
                 $tarjeta->CambiarPlus(2); //Si no tengo credito y ya use el plus1, puedo usar el plus2
-                $normaloplus="Viaje Plus";
-                $pago="";
-                $mult=0;
+                $normaloplus = "Viaje Plus";
+                $pago = "";
+                $mult = 0;
                 }
             } else {
-                if ($tarjeta->obtenerSaldo()>=$precio_efectivo) {
+                if ($tarjeta->obtenerSaldo() >= $precio_efectivo) {
                     $tarjeta->restarSaldo($precio_efectivo);
-                    $normaloplus="Normal";
-                    $pago="";
-                    $mult=1;
+                    $normaloplus = "Normal";
+                    $pago = "";
+                    $mult = 1;
                 } else {
                     $tarjeta->CambiarPlus(1);
-                    $normaloplus="Viaje Plus";
-                    $pago="";
-                    $mult=0;
+                    $normaloplus = "Viaje Plus";
+                    $pago = "";
+                    $mult = 0;
                 }
             }
         }
-        $boleto=new Boleto($precio_efectivo, $this, $tarjeta, $precio_efectivo*$mult, $normaloplus, $pago, $fecha_actual);
+        $boleto = new Boleto($precio_efectivo, $this, $tarjeta, $precio_efectivo * $mult, $normaloplus, $pago, $fecha_actual);
         $tarjeta->CambiarUltBol($boleto);
         return $boleto;
     }
 
-    public function pagarConMedio(TarjetaInterface $tarjeta, TiempoInterface $tiempo,$ultimo_boleto,$fecha_actual){
-        $fechaUlt=$ultimo_boleto->fecha;
-        if (($fecha_actual-$fechaUlt)>4) {
+    public function pagarConMedio(TarjetaInterface $tarjeta, TiempoInterface $tiempo, $ultimo_boleto, $fecha_actual) {
+        $fechaUlt = $ultimo_boleto->fecha;
+        if (($fecha_actual-$fechaUlt) > 4) {
             return 0.5;
         } else {
             return 1;
         }
     }
 
-    public function pagarConFranquiciaTotal(TarjetaInterface $tarjeta, TiempoInterface $tiempo){
-        if ($tiempo->dia!=$tarjeta->fechaViaje1) {
-            $tarjeta->fechaViaje1=$tiempo->dia;
+    public function pagarConFranquiciaTotal(TarjetaInterface $tarjeta, TiempoInterface $tiempo) {
+        if ($tiempo->dia != $tarjeta->fechaViaje1) {
+            $tarjeta->fechaViaje1 = $tiempo->dia;
             return 0;
-        } elseif ($tiempo->dia!=$tarjeta->fechaViaje2) {
-            $tarjeta->fechaViaje2=$tiempo->dia;
+        } elseif ($tiempo->dia != $tarjeta->fechaViaje2) {
+            $tarjeta->fechaViaje2 = $tiempo->dia;
             return 0;
         } else {
             return 1;
         }
     }
 
-    public function puedePagarDosPlus(TarjetaInterface $tarjeta, TiempoInterface $tiempo,$precio_efectivo){
-        if($tarjeta->obtenerPlus2()==FALSE&&$tarjeta->obtenerSaldo()>=$precio_efectivo+($tarjeta->obtenerMonto()*2)){
+    public function puedePagarDosPlus(TarjetaInterface $tarjeta, TiempoInterface $tiempo, $precio_efectivo) {
+        if ($tarjeta->obtenerPlus2() == FALSE && $tarjeta->obtenerSaldo() >= $precio_efectivo+($tarjeta->obtenerMonto() * 2)) {
             return TRUE;
         }
         return FALSE;
