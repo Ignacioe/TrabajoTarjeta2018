6c4b7477322a95c53ca7fdbc35c3d65fc209a9c8
Ignacioe <mypromega@hotmail.com>
2018-12-06 09:39:16 -0300

    Separo pagarCon() en mas funciones

--- src/ColectivoInterface.php
+++ src/ColectivoInterface.php
@@ -38,6 +38,39 @@ interface ColectivoInterface {
     public function pagarCon(TarjetaInterface $tarjeta, TiempoInterface $tiempo);
 
     /**
+     * Realiza los pasos extra que hay que hacer si se paga con una franquicia media.
+     *
+     * @param TarjetaInterface $tarjeta
+     * @param TiempoInterface $tiempo
+     *
+     * @return int
+     *  El multiplicador, 0.5 si se paga medio boleto, 1 si se paga normal.
+     */
+    public function pagarConMedio(TarjetaInterface $tarjeta, TiempoInterface $tiempo);
+
+    /**
+     * Realiza los pasos extra que hay que hacer si se paga con una franquicia total.
+     *
+     * @param TarjetaInterface $tarjeta
+     * @param TiempoInterface $tiempo
+     *
+     * @return int
+     *  El multiplicador, 0 si es gratis, 1 si se paga normal.
+     */
+    public function pagarConFranquiciaTotal(TarjetaInterface $tarjeta, TiempoInterface $tiempo);
+
+    /**
+     * Corrobora si se tiene saldo suficiete para abonar dos viajes plus mas el boleto actual.
+     *
+     * @param TarjetaInterface $tarjeta
+     * @param TiempoInterface $tiempo
+     *
+     * @return bool
+     *  TRUE si tiene saldo suficiente para pagar 2 vaijes plus mas su boleto actual, FALSE si no alcanza.
+     */
+    public function puedePagarDosPlus(TarjetaInterface $tarjeta, TiempoInterface $tiempo);
+    
+    /**
      * Devuelve si ese dia y hora son validos para trasbordo
      *
      * @param TarjetaInterface $tarjeta
